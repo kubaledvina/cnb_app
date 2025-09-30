1c2378a54586deb3a9f62e2335888d9b788c4182
Jakub Ledvina <kubaledvina@gmail.com>
2025-09-30 18:05:37 +0200

    sheme.sql

--- shema.sql
+++ shema.sql
@@ -1,9 +1,16 @@
+-- Create the database
+CREATE DATABASE cnb_rates;
+
+-- Switch to the database
+\c cnb_rates;
+
+-- Create the table
 CREATE TABLE IF NOT EXISTS exchange_rates (
-            id SERIAL PRIMARY KEY,
-            country VARCHAR(100),
-            currency VARCHAR(50),
-            amount INT,
-            code VARCHAR(10),
-            rate NUMERIC,
-            date DATE
-        );
+    id SERIAL PRIMARY KEY,
+    country VARCHAR(100),
+    currency VARCHAR(50),
+    amount INT,
+    code VARCHAR(10),
+    rate NUMERIC,
+    date DATE
+);
